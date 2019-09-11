84252f7fbc51a01af5fe227f2e32b7606a533219
Senkevich Alexsandr <a.siankevich@altoros.com>
2019-09-11 18:07:51 +0300

    bar_and_restaurant.html

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
     hot: true,
     open: true,
     port: process.env.PORT || 3000,
-    host: process.env.HOST || 'localhost',
+    host: '0.0.0.0' || 'localhost',
   },
   module: {
     rules: [
